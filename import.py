ee97168345e81a4af4085ac1b85950e000837d07
Koshish Pariyar <koshishpariyar0@gmail.com>
2025-03-23 14:48:34 +0545

    Add files via upload
    In this code a learned how to install modules using pip and how to import  and use them in my code

---
+++ import.py
@@ -0,0 +1,8 @@
+import pyjokes
+print("Printing jokes....")
+# This print a random jokes
+joke=pyjokes.get_joke()
+print(joke)
+""" in this code i learned how to install modules using pip 
+and how to import them in the code """
+  
