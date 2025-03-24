99c45f2a13386b02331df6bf16bc9b23831d04b4
Koshish Pariyar <koshishpariyar0@gmail.com>
2025-03-24 22:05:01 +0545

    Add files via upload

---
+++ Practice/Chapter1/Chapter2/problem5.py
@@ -0,0 +1,5 @@
+# To find the average between two numbers entered by user
+a=int(input("Enter the first number:"))
+b=int(input("Enter second number:"))
+avg=(a+b)/2
+print("The average between a and b is:",avg)
---
+++ Practice/Chapter1/Chapter2/problem6.py
@@ -0,0 +1,4 @@
+# To find the square of a number entered by user
+k=int(input("Enter a number:"))
+b=k*k
+print("The square of the number is:",b)
