ffe268148d7cc51de7e34447c0b8095a1be938ba
Koshish Nepali <koshishnepali@smc.edu.np>
2025-04-01 22:33:51 +0545

    Add files via upload

---
+++ break_continue.py
@@ -0,0 +1,8 @@
+for i in range(100):
+    if(i==34):
+        break #Exit the loop
+    print(i)
+for i in range(100):
+    if(i==34):
+        continue #Skip the iteration
+    print(i)
---
+++ list_while.py
@@ -0,0 +1,5 @@
+l=[1,"Harry","This","Apeak",True,"Deepika","Hello"]
+i=0
+while(i<len(l)):
+    print(l[i])
+    i+=1
